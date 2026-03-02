bd1a949be2dd7849e7ba0b56e200274f8e8cef44
Anupam Yadav <anupamyadav4347@gmail.com>
2026-03-02 22:16:20 +0530

    .

---
+++ app/src/main/java/com/visionary/jetpackcompose/jetpackcompose/scaffold/BottomBar.kt
@@ -0,0 +1,50 @@
+package com.visionary.jetpackcompose.jetpackcompose.scaffold
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.visionary.jetpackcompose.R
+
+@Preview(showSystemUi = true)
+@Composable
+fun BottomBarExample(modifier: Modifier = Modifier) {
+    BottomAppBar(
+        modifier= Modifier.fillMaxWidth(),
+
+    ) {
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround) {
+            IconButton(onClick = {}) {
+                Icon(imageVector = Icons.Default.Home, contentDescription = null)
+            }
+            IconButton(onClick = {}) {
+                Icon(imageVector = Icons.Default.Search, contentDescription = null)
+            }
+            IconButton(onClick = {}) {
+                Icon(
+                    modifier = Modifier.size(20.dp),
+                    painter = painterResource(R.drawable.img_1),
+                    contentDescription = null
+                )
+            }
+            IconButton(onClick = {}) {
+                Icon(imageVector = Icons.Default.Person, contentDescription = null)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/visionary/jetpackcompose/jetpackcompose/scaffold/FlottingActionButton.kt
@@ -0,0 +1,21 @@
+package com.visionary.jetpackcompose.jetpackcompose.scaffold
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+@Preview(showSystemUi = true)
+@Composable
+fun FlotingActionButtonExample(modifier: Modifier = Modifier) {
+    FloatingActionButton(
+        onClick = {},
+        containerColor = Color.Blue,
+        contentColor = Color.White
+    ) {
+        Icon(imageVector = Icons.Default.Add,contentDescription = null)
+    }
+}

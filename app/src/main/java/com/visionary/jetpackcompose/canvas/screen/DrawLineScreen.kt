918a8d2d4e0dad44f5575f03ffb806c411477f95
Anupam Yadav <ay846115@zohomail.in>
2026-04-04 15:16:24 +0530

    .

--- app/src/main/java/com/visionary/jetpackcompose/canvas/screen/DrawLineScreen.kt
+++ app/src/main/java/com/visionary/jetpackcompose/canvas/screen/DrawLineScreen.kt
@@ -6,9 +6,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.tooling.preview.Preview
 
-@Preview
 @Composable
 fun DrawLineScreen() {
     Canvas(modifier = Modifier.fillMaxSize()) {
@@ -16,9 +16,89 @@ fun DrawLineScreen() {
         val canvasHeight=size.height
         drawLine(
             color = Color.Red,
-            start = Offset(x=0f,y=canvasHeight/2) ,
-            end = Offset(x=canvasWidth,y=canvasHeight/2),
-            strokeWidth = 10f
+            start = Offset(x=20f,y=canvasHeight/2) ,
+            end = Offset(x=canvasWidth-20,y=canvasHeight/2),
+            strokeWidth = 10f,
+//            cap = StrokeCap.Round,
+//            alpha = 0.8f,
+            pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f,30f), phase = 70f)
         )
+    }
+}
+@Composable
+fun DiagonalLineScreen() {
+    Canvas(modifier = Modifier.fillMaxSize()) {
+        val canvasWidth=size.width
+        val canvasHeight=size.height
+        drawLine(
+            color = Color.Red,
+            start = Offset(x=0f,y=0f),
+            end = Offset(x=canvasWidth, y = canvasHeight),
+            strokeWidth = 10f,
+        )
+    }
+}
+
+@Preview
+@Composable
+fun VerticalCenterLineScreen() {
+    Canvas(modifier = Modifier.fillMaxSize()) {
+        val canvasWidth=size.width
+        val canvasHeight=size.height
+        drawLine(
+            color = Color.Red,
+            start = Offset(x=canvasWidth/2f,y=0f),
+            end = Offset(x=canvasWidth/2, y = canvasHeight),
+            strokeWidth = 10f,
+        )
+    }
+}
+
+@Composable
+fun CrossLineScreen() {
+    Canvas(modifier = Modifier.fillMaxSize()) {
+        val canvasWidth=size.width
+        val canvasHeight=size.height
+        drawLine(
+            color = Color.Red,
+            start = Offset(x=canvasWidth/2f,y=0f),
+            end = Offset(x=canvasWidth/2, y = canvasHeight),
+            strokeWidth = 10f,
+        )
+        drawLine(
+            color = Color.Red,
+            start = Offset(x=0f,y=canvasHeight/2f),
+            end = Offset(x=canvasWidth, y = canvasHeight/2f),
+            strokeWidth = 10f,
+        )
+
+    }
+}
+
+@Preview
+@Composable
+fun KotlinLogo() {
+    Canvas(modifier = Modifier.fillMaxSize()) {
+        val canvasWidth=size.width
+        val canvasHeight=size.height
+        drawLine(
+            color = Color.Blue,
+            start = Offset(x=canvasWidth/2-200, y = canvasHeight/2-250),
+            end =Offset(x=canvasWidth/2-200,y=canvasHeight/2+250) ,
+            strokeWidth = 10f,
+        )
+        drawLine(
+            color = Color.Blue,
+            start = Offset(x=canvasWidth/2-200, y = canvasHeight/2-250),
+            end = Offset(x=canvasWidth/2+200,y=canvasHeight/2-250),
+            strokeWidth = 10f,
+        )
+        drawLine(
+            color = Color.Blue,
+            start = Offset(x=canvasWidth/2+200, y = canvasHeight/2+250),
+            end = Offset(x=canvasWidth/2-200,y=canvasHeight/2+250),
+            strokeWidth = 10f,
+        )
+
     }
 }
